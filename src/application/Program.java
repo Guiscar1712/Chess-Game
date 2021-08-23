bbb9877261231f1ef9591f5d72f6931c03702fd9
Guilherme Scaramella <guiscar1712@gmail.com>
2021-08-23 12:35:45 -0300

    Chess

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,53 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+
+        ChessMatch chessMatch = new ChessMatch();
+        Scanner scanner = new Scanner(System.in);
+        List<ChessPiece> captured = new ArrayList<ChessPiece>();
+
+        while (!chessMatch.getCheckMate()) {
+            try {
+                UI.clearScreen();
+                UI.printMatch(chessMatch, captured);
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(scanner);
+
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(scanner);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+                if (capturedPiece != null) {
+                    captured.add(capturedPiece);
+                }
+            }
+            catch (ChessException error) {
+                System.out.println(error.getMessage());
+                scanner.nextLine();
+            } catch (InputMismatchException error) {
+                System.out.println(error.getMessage());
+                scanner.nextLine();
+            }
+        }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,36 @@
+package boardgame;
+
+import chess.Color;
+
+public abstract class Piece {
+    protected Position position;
+    private Board board;
+
+    public abstract boolean[][] possibleMoves ();
+
+    public boolean possibleMove (Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove () {
+        boolean[][] mat = possibleMoves();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (mat[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+    public abstract Color getColor();
+}
